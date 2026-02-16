9acf4df7128498bd7840a2996cc7b5e09c11165c
Florin Latcu (Z0245162) <florin.latcu@zf.com>
2026-02-16 14:59:17 +0200

    Added comment to Two_Sum

--- cargo/src/two_sum_leetcode1.rs
+++ cargo/src/two_sum_leetcode1.rs
@@ -1,3 +1,11 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order. 
+*/
+
 use std::collections::HashMap;
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     //  Create a hash map to store value -> index
